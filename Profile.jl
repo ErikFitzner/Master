f30ab39a9fdc483085423a92bbebf90830dc1754
ErikFitzner <137158922+ErikFitzner@users.noreply.github.com>
2025-08-24 11:08:33 +0200

    Delete Profile.jl

--- Profile.jl
+++
@@ -1,5 +0,0 @@
-using FlameGraphs, FileIO, ProfileView
-
-data, lidict = load("C:/Users/User/Downloads/Profile_DSF.jlprof")   # tuple zurück
-g = flamegraph(data; lidict)                   # Flame-Graph bauen
-ProfileView.view(g)                            # interaktiv inspizieren
